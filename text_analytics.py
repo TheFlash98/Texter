7315f6a0ef505a3d8f19411177251e5094f029a7
TheFlash98 <mastermind.sarthak@gmail.com>
2018-01-03 02:04:59 +0530

    Data from server

--- text_analytics.py
+++ text_analytics.py
@@ -1,15 +1,15 @@
 ########### Python 2.7 #############
 import requests, json
 
-data_file = open("data.txt","r")
-
+#data_file = open("data.txt","r")
+data = request.data['textcontent']
 url = 'https://southeastasia.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases'
 payload = {
     # Request parameters
     "documents": [
     {
       "id": "string",
-      "text": "Many Deep Learning hardware startup ventures will begin to finally deliver their silicon in 2018. These will all be mostly busts because they will forget to deliver good software to support their new solutions. These firms have as their DNA, hardware. Unfortunately, in the DL space, software is just as important. Most of these startups don’t understand software and don’t understand the cost of developing software. These firms may deliver silicon, but nothing will ever run on them!"
+      "text": data    
     }
   ]
 }
