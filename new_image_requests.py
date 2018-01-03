8b30fbe37e264cf8b9ab5bcae4157f236cd5050a
TheFlash98 <mastermind.sarthak@gmail.com>
2018-01-03 06:03:51 +0530

    Loop over queries

--- new_image_requests.py
+++ new_image_requests.py
@@ -3,12 +3,14 @@ import requests, json
 
 #data_file = open("data.txt","r")
 #data = request.data['textcontent']
+my_list = []
 url = 'https://api.cognitive.microsoft.com/bing/v7.0/images/search'
 headers = {
     'Ocp-Apim-Subscription-Key': '60fc159801924ff89e38e80ebd688e26'
 }
-query = 'cat'
-response = requests.get(url+"?q=" + query+"&count=1", headers=headers)
-data = response.json()
-if data['value']:
-    print data['value'][0]['contentUrl']
+for query in queries:
+    response = requests.get(url+"?q=" + query+"&count=1", headers=headers)
+    data = response.json()
+    if data['value']:
+        content_url =  data['value'][0]['contentUrl']
+        my_list.append(content_url)
